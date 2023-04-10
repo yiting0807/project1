a1fe1de7fb0e83fd30e7a1faaa67b7aa5dcdcd66
yiting0807 <anniewang0807@gmail.com>
2023-04-11 06:02:42 +0800

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
 import Japan from './pages/Japan'
-import Login from './pages/Login'
 import Slider from "react-slick";
 import {
   BrowserRouter,Routes,Route,Link
@@ -16,11 +15,9 @@ function App() {
     <BrowserRouter>
     <Link to='/'><img src="images/yt.png" width="10px"/></Link>
     <Link to='/japan'>japan</Link>
-    {/* <Link to='/home1'>login</Link> */}
       <Routes>
        <Route path="/" element={<Home/>}/>
        <Route path="/japan" element={<Japan/>}/>
-       {/* <Route path="/home1" element={<Home1/>}/>  */}
      {/* <Home/> */}
        
     </Routes>
