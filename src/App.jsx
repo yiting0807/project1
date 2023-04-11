0059ea9cdcfc10dd3697870ef673df216ad5cf34
yiting0807 <anniewang0807@gmail.com>
2023-04-11 09:58:20 +0800

    loginpage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
 import Japan from './pages/Japan'
+import First from './pages/First'
 import Slider from "react-slick";
 import {
   BrowserRouter,Routes,Route,Link
@@ -15,9 +16,11 @@ function App() {
     <BrowserRouter>
     <Link to='/'><img src="images/yt.png" width="10px"/></Link>
     <Link to='/japan'>japan</Link>
+    <Link to='/first'>First</Link>
       <Routes>
        <Route path="/" element={<Home/>}/>
        <Route path="/japan" element={<Japan/>}/>
+       <Route path="/first" element={<First/>}/>
      {/* <Home/> */}
        
     </Routes>
---
+++ src/pages/First.jsx
@@ -0,0 +1,16 @@
+import Headerthr from "../compenents/Headerthr";
+import Footertwo from "../compenents/footertwo"
+import ProductList from "../compenents/ProductList";
+
+function First() {
+    return (
+      <div className="container">
+        <Headerthr/>
+        <Footertwo/>
+        <ProductList/>
+      </div>
+    );
+}
+
+    
+export default First;
