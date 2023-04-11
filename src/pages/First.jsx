340966c3a30f9b7e209107aa776b4f420f7260e0
yiting0807 <anniewang0807@gmail.com>
2023-04-11 10:03:40 +0800

    final

--- src/pages/First.jsx
+++ src/pages/First.jsx
@@ -1,13 +1,13 @@
 import Headerthr from "../compenents/Headerthr";
-import Footertwo from "../compenents/footertwo"
+import Footertwo from "../compenents/footertwo";
 import ProductList from "../compenents/ProductList";
 
 function First() {
     return (
       <div className="container">
         <Headerthr/>
-        <Footertwo/>
         <ProductList/>
+        <Footertwo/>
       </div>
     );
 }
